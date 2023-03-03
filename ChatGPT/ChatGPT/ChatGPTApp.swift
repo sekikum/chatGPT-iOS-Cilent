ced5d6657f4c28b9d0e79bdb9dc2a3da99f9dc6f
Wenyan Zhao <sekikumxna@163.com>
2023-03-03 15:31:18 +0800

    core: create project

---
+++ ChatGPT/ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Wenyan Zhao on 2023/3/3.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
