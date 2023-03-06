c17e57ba7f5e27a41af5881504679501a95a3b8a
Wenyan Zhao <wenyan.zhao@thoughtworks.com>
2023-03-06 17:06:00 +0800

    refactor: mock message data as real

--- ChatGPT/ChatGPT/View/MessageView.swift
+++ ChatGPT/ChatGPT/View/MessageView.swift
@@ -42,8 +42,7 @@ struct MessageView: View {
 
 struct MessageView_Previews: PreviewProvider {
   static var previews: some View {
-    MessageView(message: MessageModel(message: "In the darkIn the darkIn the darkIn the darkIn the darkIn the darkIn the darkIn the darkIn the darkIn the darkIn the dark", isUser: true))
-    MessageView(message: MessageModel(message: "", isUser: true))
-    MessageView(message: MessageModel(message: "Viper ViperViper ViperViper ViperViper ViperViper ViperViper ViperViper ViperViper ViperViper ViperViper ViperViper ViperViper ViperViper ViperViper Viper", isUser: false))
+    MessageView(message: MessageModel(message: "mysql输入框怎么退出", isUser: true))
+    MessageView(message: MessageModel(message: "在MySQL命令行输入框中，如果您想要退出，请执行以下步骤：\n1. 输入quit并按下回车键。(如果您正在编辑或输入一条命令，按下Ctrl+C可以取消命令并返回到命令行提示符)\n2. 如果您需要强制退出，请按下Ctrl+D键，这将关闭MySQL命令行并返回到系统命令行提示符。\n请注意，以上步骤可能会因使用的操作系统和MySQL版本而略有不同。", isUser: false))
   }
 }
