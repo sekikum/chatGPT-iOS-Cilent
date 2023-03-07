3c1864936615c3c7163473bf2af5f4b1b06562f9
Wenyan Zhao <wenyan.zhao@thoughtworks.com>
2023-03-07 17:02:22 +0800

    feat: modify textField style to adapt dark mode

--- ChatGPT/ChatGPT/View/InputView.swift
+++ ChatGPT/ChatGPT/View/InputView.swift
@@ -10,11 +10,15 @@ import SwiftUI
 
 struct InputView: View {
   @State var textfieldText: String = ""
+  let padding: CGFloat = 6
+  let cornerRadius: CGFloat = 6
   
   var body: some View {
     HStack {
       TextField("", text: $textfieldText)
-        .textFieldStyle(RoundedBorderTextFieldStyle())
+        .padding(padding)
+        .background(Color("Gray"))
+        .cornerRadius(cornerRadius)
         .keyboardType(.default)
       Button("send") {
         print(textfieldText)
