3cd77caa76d329bde9ec7c0fddf71c1632cbe8ec
Wenyan Zhao <sekikumxna@163.com>
2023-03-07 15:18:34 +0800

    refactor: use keyboardPublisher and scrollviewReader to scroll to bottom when keyboard appear

--- ChatGPT/ChatGPT/ChatGPTApp.swift
+++ ChatGPT/ChatGPT/ChatGPTApp.swift
@@ -6,20 +6,9 @@
 //
 
 import SwiftUI
-import IQKeyboardManagerSwift
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    IQKeyboardManager.shared.enable = true
-    IQKeyboardManager.shared.shouldResignOnTouchOutside = true
-    return true
-  }
-}
 
 @main
 struct ChatGPTApp: App {
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-  
   var body: some Scene {
     WindowGroup {
       ContentView()
---
+++ ChatGPT/ChatGPT/KeyBoardPublisher.swift
@@ -0,0 +1,27 @@
+//
+//  KeyBoardPublisher.swift
+//  ChatGPT
+//
+//  Created by Wenyan Zhao on 2023/3/7.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+extension View {
+  var keyboardPublisher: AnyPublisher<Bool, Never> {
+    Publishers
+      .Merge(
+        NotificationCenter
+          .default
+          .publisher(for: UIResponder.keyboardWillShowNotification)
+          .map { _ in true },
+        NotificationCenter
+          .default
+          .publisher(for: UIResponder.keyboardWillHideNotification)
+          .map { _ in false })
+      .debounce(for: .seconds(0.1), scheduler: RunLoop.main)
+      .eraseToAnyPublisher()
+  }
+}
