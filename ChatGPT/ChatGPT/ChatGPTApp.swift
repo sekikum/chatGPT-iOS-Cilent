e54173c98b8cc3d055071605e9ef1fa722839553
Wenyan Zhao <sekikumxna@163.com>
2023-03-07 14:03:04 +0800

    feat: move scrollview up when keyboard appear & reset scrollview when keyboard disappear

--- ChatGPT/ChatGPT/ChatGPTApp.swift
+++ ChatGPT/ChatGPT/ChatGPTApp.swift
@@ -6,9 +6,19 @@
 //
 
 import SwiftUI
+import IQKeyboardManagerSwift
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    IQKeyboardManager.shared.enable = true
+    return true
+  }
+}
 
 @main
 struct ChatGPTApp: App {
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+  
   var body: some Scene {
     WindowGroup {
       ContentView()
