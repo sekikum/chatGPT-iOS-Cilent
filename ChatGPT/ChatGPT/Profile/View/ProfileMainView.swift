f102034471365cd286cf7be4d6f7d1373791c7e7
Wenyan Zhao <sekikumxna@163.com>
2023-03-09 17:35:55 +0800

    feat: add new token

--- ChatGPT/ChatGPT/Profile/View/ProfileMainView.swift
+++ ChatGPT/ChatGPT/Profile/View/ProfileMainView.swift
@@ -11,6 +11,8 @@ import SwiftUI
 struct ProfileMainView: View {
   @EnvironmentObject var homeViewModel: HomeViewModel
   @State var selection: String = ""
+  @State var textfieldText: String = ""
+  @State var isShowAlert: Bool = false
   
   var body: some View {
     List {
@@ -30,11 +32,31 @@ struct ProfileMainView: View {
           .labelsHidden()
         }
       }
+      
+      Section {
+        HStack {
+          TextField("input new token", text: $textfieldText)
+            .keyboardType(.default)
+            .submitLabel(.done)
+            .onSubmit(addNewToken)
+          Button("send", action: addNewToken)
+            .buttonStyle(.borderedProminent)
+            .alert("token cannot be empty", isPresented: $isShowAlert) {
+              Button("OK", role: .cancel) { }
+            }
+        }
+      }
     }
   }
   
-  func setSelectedToken() {
-    homeViewModel.user.tokenSelect = selection
+  func addNewToken() {
+    if textfieldText.isEmpty {
+      isShowAlert = true
+      return
+    }
+    homeViewModel.user.tokenList.append(textfieldText)
+    textfieldText = ""
+    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
   }
 }
 
