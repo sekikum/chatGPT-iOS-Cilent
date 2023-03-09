6fb41f1cb53d4cc077e27da26efccf9957e85407
Wenyan Zhao <wenyan.zhao@thoughtworks.com>
2023-03-09 14:38:16 +0800

    feat: add profileMainView

---
+++ ChatGPT/ChatGPT/Profile/ProfileMainView.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileMainView.swift
+//  ChatGPT
+//
+//  Created by Wenyan Zhao on 2023/3/9.
+//
+
+import Foundation
+import SwiftUI
+
+struct ProfileMainView: View {
+  var body: some View {
+    Text("this is ProfileView")
+  }
+}
