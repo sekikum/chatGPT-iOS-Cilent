b5b196520016ec02345c16aee2eb667ab4cb068d
Wenyan Zhao <wenyan.zhao@thoughtworks.com>
2023-03-09 15:19:23 +0800

    refactor: add user info and set up as environment object

---
+++ ChatGPT/ChatGPT/Home/Model/UserModel.swift
@@ -0,0 +1,15 @@
+//
+//  UserModel.swift
+//  ChatGPT
+//
+//  Created by Wenyan Zhao on 2023/3/9.
+//
+
+import Foundation
+
+struct UserModel {
+  var avatar: String
+  var nickname: String
+  var tokenList: [String]
+  var tokenSelect: String?
+}
