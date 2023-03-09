533d956bc291bd84e9b59d780f5397505f564198
Wenyan Zhao <sekikumxna@163.com>
2023-03-09 16:03:59 +0800

    feat: show token list on profile view

--- ChatGPT/ChatGPT/Profile/View/ProfileMainView.swift
+++ ChatGPT/ChatGPT/Profile/View/ProfileMainView.swift
@@ -9,10 +9,19 @@ import Foundation
 import SwiftUI
 
 struct ProfileMainView: View {
+  @EnvironmentObject var homeViewModel: HomeViewModel
+  
   var body: some View {
     List {
       Section {
-        ProfileHeaderView()
+        ProfileHeaderView(avatar: homeViewModel.user.avatar, nickname: homeViewModel.user.nickname)
+      }
+      
+      Section {
+        ForEach(homeViewModel.user.tokenList, id: \.self) { token in
+          Text(token)
+            .lineLimit(1)
+        }
       }
     }
   }
