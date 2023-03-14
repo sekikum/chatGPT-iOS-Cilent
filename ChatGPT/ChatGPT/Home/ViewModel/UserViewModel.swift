929461d771a90cba3a8aaade008444b3a51b3ce5
Wenyan Zhao <sekikumxna@163.com>
2023-03-14 18:10:12 +0800

    feat: send message to chatGPT API and show answer

--- ChatGPT/ChatGPT/Home/ViewModel/UserViewModel.swift
+++ ChatGPT/ChatGPT/Home/ViewModel/UserViewModel.swift
@@ -7,28 +7,10 @@
 
 import Foundation
 
-@MainActor class UserViewModel: ObservableObject {
-  @Published var user: UserModel = UserModel(avatar: "Profile-Diu", nickname: "sekikum", tokenList: [], tokenSelect: "")
-  
-  private static var UsersURL: URL {
-    let path = NSHomeDirectory() + "/users.data"
-    return URL(fileURLWithPath: path)
-  }
+class UserViewModel: ObservableObject {
+  @Published var user: UserModel
   
   init() {
-    guard let data = UserDefaults.standard.data(forKey: "USER") else {
-      return
-    }
-    guard let userStored = try? PropertyListDecoder().decode(UserModel.self, from: data) else {
-      return
-    }
-    user = userStored
-  }
-  
-  func storeUser(_ user: UserModel) async {
-    guard let data = try? PropertyListEncoder().encode(user) else {
-      return
-    }
-    UserDefaults.standard.set(data, forKey: "USER")
+    user = StorageManager.restoreUser()
   }
 }
