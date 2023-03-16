df40932c1d62d5b97ccdde51d3ce9171cd52851f
Wenyan Zhao <sekikumxna@163.com>
2023-03-16 16:43:44 +0800

    feat: cannot input message when token list is empty

--- ChatGPT/ChatGPT/Home/View/HomeView.swift
+++ ChatGPT/ChatGPT/Home/View/HomeView.swift
@@ -8,18 +8,18 @@
 import SwiftUI
 
 struct HomeView: View {
-  @StateObject var viewModel: UserViewModel = UserViewModel()
+  @StateObject var userViewModel: UserViewModel = UserViewModel()
   @StateObject var messageViewModel: MessageViewModel = MessageViewModel()
   @State var selectionTab: HomeTab = .chat
   
   var body: some View {
     TabView(selection: $selectionTab) {
-      ChatMainView(viewModel: messageViewModel, avatar: viewModel.user.avatar)
+      ChatMainView(viewModel: messageViewModel, noTokenAdded: $userViewModel.noTokenAdded, avatar: userViewModel.user.avatar)
         .tabItem {
           Label("Chat", systemImage: "message.fill")
         }
         .tag(HomeTab.chat)
-      ProfileMainView(viewModel: viewModel, initToken: messageViewModel.initOpenAI)
+      ProfileMainView(viewModel: userViewModel, noTokenAdded: $userViewModel.noTokenAdded, initToken: messageViewModel.initOpenAI)
         .tabItem {
           Label("Me", systemImage: "person.fill")
         }
