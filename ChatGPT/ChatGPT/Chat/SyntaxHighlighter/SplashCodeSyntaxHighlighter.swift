d38b0fae25177d44c94b1410530ad7fd8b903891
Wenyan Zhao <sekikumxna@163.com>
2023-03-17 09:59:45 +0800

    feat: message support markdown

---
+++ ChatGPT/ChatGPT/Chat/SyntaxHighlighter/SplashCodeSyntaxHighlighter.swift
@@ -0,0 +1,28 @@
+//
+//  SplashCodeSyntaxHighlighter.swift
+//  ChatGPT
+//
+//  Created by Wenyan Zhao on 2023/3/17.
+//
+
+import MarkdownUI
+import Splash
+import SwiftUI
+
+struct SplashCodeSyntaxHighlighter: CodeSyntaxHighlighter {
+  private let syntaxHighlighter: SyntaxHighlighter<TextOutputFormat>
+
+  init(theme: Splash.Theme) {
+    self.syntaxHighlighter = SyntaxHighlighter(format: TextOutputFormat(theme: theme))
+  }
+
+  func highlightCode(_ content: String, language: String?) -> Text {
+    return self.syntaxHighlighter.highlight(content)
+  }
+}
+
+extension CodeSyntaxHighlighter where Self == SplashCodeSyntaxHighlighter {
+  static func splash(theme: Splash.Theme) -> Self {
+    SplashCodeSyntaxHighlighter(theme: theme)
+  }
+}
