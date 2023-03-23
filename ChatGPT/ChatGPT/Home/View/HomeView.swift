f65440bcc0f2a0ca369a1ded15deb9b3602776d0
Wenyan Zhao <wenyan.zhao@thoughtworks.com>
2023-03-23 09:59:39 +0800

    feat: remove image view

--- ChatGPT/ChatGPT/Home/View/HomeView.swift
+++ ChatGPT/ChatGPT/Home/View/HomeView.swift
@@ -31,12 +31,6 @@ struct HomeView: View {
       }
       .tag(HomeTab.chat)
       
-      ImageChatMainView()
-        .tabItem {
-          Label("Image", systemImage: "photo.circle.fill")
-        }
-        .tag(HomeTab.image)
-      
       ProfileMainView(viewModel: userViewModel, initToken: messageViewModel.initOpenAI)
         .tabItem {
           Label("Me", systemImage: "person.fill")
@@ -49,7 +43,6 @@ struct HomeView: View {
 enum HomeTab {
   case chat
   case me
-  case image
 }
 
 struct HomeView_Previews: PreviewProvider {
