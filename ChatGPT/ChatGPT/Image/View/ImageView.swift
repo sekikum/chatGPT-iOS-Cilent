5b6026313bfa752effd71e55ead04b8bc961b168
Wenyan Zhao <wenyan.zhao@thoughtworks.com>
2023-03-28 14:24:22 +0800

    refactor: use asyncimage instead of image

--- ChatGPT/ChatGPT/Image/View/ImageView.swift
+++ ChatGPT/ChatGPT/Image/View/ImageView.swift
@@ -14,31 +14,50 @@ struct ImageView: View {
   let imageSingleSize: CGFloat = UIScreen.main.bounds.size.width - 60
   let cornerRadius: CGFloat = 10
   let shadowRadius: CGFloat = 5
-  let imageNames10: [String] = ["person", "cloud", "person.fill", "lasso", "trash", "trash.fill", "cloud.fill", "folder", "folder.fill"]
-  let imageNames6: [String] = ["lasso", "trash", "trash.fill", "cloud.fill", "folder", "folder.fill"]
+  let urlImages4: [String] = [
+    "https://tw-mobile-xian.github.io/moments-data/images/user/avatar/004.jpeg",
+    "https://tw-mobile-xian.github.io/moments-data/images/user/avatar/003.jpeg",
+    "https://tw-mobile-xian.github.io/moments-data/images/user/avatar/002.jpeg",
+    "https://tw-mobile-xian.github.io/moments-data/images/user/avatar/001.jpeg",
+  ]
+  let urlImages9: [String] = [
+    "https://tw-mobile-xian.github.io/moments-data/images/user/avatar/004.jpeg",
+    "https://tw-mobile-xian.github.io/moments-data/images/user/avatar/003.jpeg",
+    "https://tw-mobile-xian.github.io/moments-data/images/user/avatar/002.jpeg",
+    "https://tw-mobile-xian.github.io/moments-data/images/user/avatar/001.jpeg",
+    "https://tw-mobile-xian.github.io/moments-data/images/user/avatar/000.jpeg",
+    "https://tw-mobile-xian.github.io/moments-data/images/user/avatar/005.jpeg",
+    "https://tw-mobile-xian.github.io/moments-data/images/user/avatar/006.jpeg",
+    "https://tw-mobile-xian.github.io/moments-data/images/user/avatar/007.jpeg",
+    "https://tw-mobile-xian.github.io/moments-data/images/user/avatar/008.jpeg",
+  ]
   
   var body: some View {
     if number <= 6 {
       VStack {
         Spacer()
         if number == 1 {
-          Image(systemName: "photo")
-            .resizable()
-            .scaledToFit()
-            .frame(width: imageSingleSize, height: imageSingleSize)
-            .background(Color.gray)
-            .cornerRadius(cornerRadius)
-            .shadow(radius: shadowRadius)
-            .padding(.leading, singleImagePadding)
-            .padding(.trailing, singleImagePadding)
+          AsyncImage(url: URL(string: "https://tw-mobile-xian.github.io/moments-data/images/user/avatar/008.jpeg")) { image in
+            image
+              .resizable()
+              .scaledToFit()
+              .frame(width: imageSingleSize, height: imageSingleSize)
+              .background(Color.gray)
+              .cornerRadius(cornerRadius)
+              .shadow(radius: shadowRadius)
+              .padding(.leading, singleImagePadding)
+              .padding(.trailing, singleImagePadding)
+          } placeholder: {
+            ProgressView("Loading")
+          }
         } else if number <= 6 {
-          ImageMultipleView(imageNames: imageNames6)
+          ImageMultipleView(imagesURL: urlImages4)
         }
         Spacer()
       }
     } else {
       ScrollView {
-        ImageMultipleView(imageNames: imageNames10)
+        ImageMultipleView(imagesURL: urlImages9)
       }
       .padding(.top, scrollViewPadding)
     }
