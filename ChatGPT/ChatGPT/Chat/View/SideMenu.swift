d80c85e5180873a489155332c2687942cedb52ca
dongyu.cai <458498007@qq.com>
2023-03-30 10:55:41 +0800

    add dialogbar to show more chatview

---
+++ ChatGPT/ChatGPT/Chat/View/SideMenu.swift
@@ -0,0 +1,37 @@
+//
+//  SideMenu.swift
+//  ChatGPT
+//
+//  Created by cai dongyu on 2023/3/29.
+//
+
+import SwiftUI
+
+struct SideMenu: View {
+    @Binding var isShowing: Bool
+    var content: AnyView
+    var edgeTransition: AnyTransition = .move(edge: .leading)
+    var body: some View {
+      ZStack(alignment: .bottom) {
+        if (isShowing) {
+          Color.black
+            .opacity(0.8)
+            .onTapGesture {
+              isShowing.toggle()
+            }
+          content
+            .transition(edgeTransition)
+            .background(
+              Color.clear
+            )
+        }
+      }
+      .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+      .ignoresSafeArea()
+      .animation(.easeInOut, value: isShowing)
+    }
+  }
+
+
+
+
