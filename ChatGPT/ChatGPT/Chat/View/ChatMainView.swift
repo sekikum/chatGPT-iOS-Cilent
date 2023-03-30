3ba60253432cee53a0f91f0cc1234249343632a5
dongyu.cai <458498007@qq.com>
2023-03-30 10:56:39 +0800

    add dialogbar to show more chatview

--- ChatGPT/ChatGPT/Chat/View/ChatMainView.swift
+++ ChatGPT/ChatGPT/Chat/View/ChatMainView.swift
@@ -8,19 +8,42 @@
 import SwiftUI
 
 struct ChatMainView: View {
-  @StateObject var viewModel: MessageViewModel
+  @EnvironmentObject var viewModel: MessageViewModel
   let avatar: String
+  @Binding var presentSideMenu: Bool
   
   var body: some View {
     VStack {
-      ChatView(avatar: avatar, messageItems: viewModel.messageItems)
+      ChatView(avatar: avatar)
+        .environmentObject(viewModel)
       InputView(isShowAlert: $viewModel.isShowAlert, alertInfo: viewModel.alertInfo, send: viewModel.sendMessage, isShowLoading: viewModel.isShowLoading)
     }
+    .navigationBarItems(leading: menuButton())
+  }
+
+  func menuButton() -> some View {
+      VStack{
+          HStack{
+              Button{
+                  presentSideMenu.toggle()
+              } label: {
+                  Image(systemName: "sidebar.left")
+                      .resizable()
+                      .frame(width: 30, height: 24)
+                      .padding(.top, 20)
+              }
+              Spacer()
+          }
+
+          Spacer()
+          Spacer()
+      }
+      .padding(.horizontal, 24)
   }
 }
 
 struct ChatMainView_Previews: PreviewProvider {
   static var previews: some View {
-    ChatMainView(viewModel: MessageViewModel(), avatar: "Profile-User")
+    ChatMainView(avatar: "Profile-User", presentSideMenu: .constant(true))
   }
 }
