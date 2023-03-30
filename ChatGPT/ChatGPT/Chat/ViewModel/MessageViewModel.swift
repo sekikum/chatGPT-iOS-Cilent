e500dc232bbc3197696c8beec915f97f4365d59d
Wenyan Zhao <sekikumxna@163.com>
2023-03-30 15:58:26 +0800

    refactor: raname Token -> APIKey in strings

--- ChatGPT/ChatGPT/Chat/ViewModel/MessageViewModel.swift
+++ ChatGPT/ChatGPT/Chat/ViewModel/MessageViewModel.swift
@@ -76,8 +76,7 @@ class MessageViewModel: ObservableObject {
         DispatchQueue.main.async {
           self.isShowLoading = false
           self.isShowAlert = true
-          self.alertInfo = NSLocalizedString("Please choose the correct token and BaseURL", comment: "")
-          
+          self.alertInfo = NSLocalizedString("Please choose the correct APIKey and BaseURL", comment: "")
         }
       case .success(let success):
         DispatchQueue.main.async {
