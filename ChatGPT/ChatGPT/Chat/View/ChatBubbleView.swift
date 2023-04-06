e511f833ecf7132861269ebe082cf197c848e076
dongyu.cai <458498007@qq.com>
2023-04-06 14:47:34 +0800

    feat: add chat bubble into message view

--- ChatGPT/ChatGPT/Chat/View/ChatBubbleView.swift
+++ ChatGPT/ChatGPT/Chat/View/ChatBubbleView.swift
@@ -6,55 +6,57 @@
 //
 
 import Foundation
+import SwiftUI
 
-enum BubblePosition {
-  case right
+enum Direction {
   case left
+  case right
 }
 
 struct ChatBubble<Content>: View where Content: View {
-  let position: BubblePosition
+  let triangleDirection: Direction
   let color : Color
   let paddingSize: CGFloat = 10
   let spacing: CGFloat = 0
-  let offsetX: CGFloat = 9
+  let imageOffsetX: CGFloat = 9
   let angleDegrees: CGFloat = 180
   let content: () -> Content
   let cornerRadiu: CGFloat = 5
   let triHeight: CGFloat = 5
   let avatar: CGFloat
-  init(position: BubblePosition,
+  init(triangleDirection: Direction,
        color: Color,
        avatar: CGFloat,
        @ViewBuilder content: @escaping () -> Content) {
-    self.position = position
+    self.triangleDirection = triangleDirection
     self.color = color
     self.content = content
     self.avatar = avatar
   }
-
+  
   var body: some View {
     HStack(alignment: .top, spacing: spacing ) {
-      if position == .left {
+      if triangleDirection == .left {
         Image(systemName: "arrowtriangle.left.fill")
           .frame(height: triHeight)
           .foregroundColor(color)
-          .offset(x: offsetX)
+          .offset(x: imageOffsetX)
           .offset(y: 0)
       }
       content()
         .foregroundColor(Color.white)
         .background(color)
         .clipShape(RoundedRectangle(cornerRadius: cornerRadiu))
-
-      if position == .right {
+      
+      if triangleDirection == .right {
         Image(systemName: "arrowtriangle.left.fill")
           .frame(height: triHeight)
           .foregroundColor(color)
           .rotationEffect(Angle(degrees: angleDegrees))
-          .offset(x: -offsetX)
+          .offset(x: -imageOffsetX)
           .offset(y: (avatar - triHeight) / 2)
       }
     }
-    .padding(position == .left ? .leading : .trailing , paddingSize)
-
+    .padding(triangleDirection == .left ? .leading : .trailing , paddingSize)
+  }
+}
