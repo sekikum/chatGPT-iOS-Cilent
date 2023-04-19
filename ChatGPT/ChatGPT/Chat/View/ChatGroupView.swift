4be5f1a21d8599fcc31d4f7dc96d9835ca245757
dongyu.cai <458498007@qq.com>
2023-04-19 11:05:38 +0800

    feat: refactor chatgroup view

---
+++ ChatGPT/ChatGPT/Chat/View/ChatGroupView.swift
@@ -0,0 +1,53 @@
+//
+//  ChatGroupView.swift
+//  ChatGPT
+//
+//  Created by cai dongyu on 2023/4/18.
+//
+
+import SwiftUI
+
+/// Hide the navigation back button title
+//extension UINavigationController {
+//  open override func viewWillLayoutSubviews() {
+//      super.viewWillLayoutSubviews()
+//    navigationBar.topItem?.backButtonDisplayMode = .minimal
+//    }
+//  }
+
+struct ChatGroupView: View {
+  @StateObject var viewModel: MessageViewModel
+  let avatar: String
+
+  var body: some View {
+    NavigationStack {
+      List(viewModel.chatGroups, id: \.self) { group in
+        if let flag = group.flag {
+          NavigationLink {
+            ChatMainView(viewModel: viewModel, avatar: avatar, group: group)
+          } label: {
+            Label(flag, systemImage: "bubble.left")
+              .font(.system(.title2))
+          }
+          .padding(.vertical, 8)
+        }
+      }
+      .navigationTitle("Chat")
+      .navigationBarTitleDisplayMode(.inline)
+      .toolbar {
+        ToolbarItem(placement: .navigationBarTrailing) {
+          NavigationLink(destination: ChatMainView(viewModel: viewModel, avatar: avatar, group: nil, isCreateGroup: true)) {
+            Image(systemName: "plus")
+              .resizable()
+          }
+        }
+      }
+    }
+  }
+}
+
+struct ChatGroupView_Previews: PreviewProvider {
+  static var previews: some View {
+    ChatGroupView(viewModel: MessageViewModel(), avatar: "1")
+  }
+}
