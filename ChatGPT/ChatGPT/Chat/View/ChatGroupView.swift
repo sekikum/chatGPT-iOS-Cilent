9673d203dca340d218f7b5f2ba6bf814c1ee3fe1
Wenyan Zhao <sekikumxna@163.com>
2023-05-08 17:34:37 +0800

    refactor: rename group -> chatGroup

--- ChatGPT/ChatGPT/Chat/View/ChatGroupView.swift
+++ ChatGPT/ChatGPT/Chat/View/ChatGroupView.swift
@@ -28,7 +28,7 @@ struct ChatGroupView: View {
         }
         .onDelete { indexSet in
           for index in indexSet {
-            viewModel.deleteGroup(index)
+            viewModel.deleteChatGroup(index)
           }
         }
       }
@@ -44,7 +44,7 @@ struct ChatGroupView: View {
         ToolbarItem(placement: .navigationBarTrailing) {
           Button(action: {
             navigateToNewGroup = true
-            viewModel.addGroup()
+            viewModel.addChatGroup()
           }) {
             Image(systemName: "plus")
               .resizable()
