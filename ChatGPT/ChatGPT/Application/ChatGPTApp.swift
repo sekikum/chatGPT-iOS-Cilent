db9a01e39bee0f11fa38ad1634c3def7ef8bbe9f
Wenyan Zhao <sekikumxna@163.com>
2023-08-21 15:55:03 +0800

    feat: add quick atcion on start new chat

--- ChatGPT/ChatGPT/Application/ChatGPTApp.swift
+++ ChatGPT/ChatGPT/Application/ChatGPTApp.swift
@@ -6,12 +6,60 @@
 //
 
 import SwiftUI
+import UIKit
+
+let quickActionSettings = QuickActionSettings()
+var shortcutItemToProcess: UIApplicationShortcutItem?
 
 @main
 struct ChatGPTApp: App {
+  @Environment(\.scenePhase) var phase
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+  
   var body: some Scene {
     WindowGroup {
       HomeView()
+        .environmentObject(quickActionSettings)
     }
+    .onChange(of: phase) { (newPhase) in
+      switch newPhase {
+      case .active:
+        guard let name = shortcutItemToProcess?.userInfo?["name"] as? String else {
+          return
+        }
+        switch name {
+        case "chat":
+          quickActionSettings.quickAction = .newChat
+        default:
+          break
+        }
+        shortcutItemToProcess = nil
+      case .inactive:
+        break
+      case .background:
+        quickActionSettings.addQuickActions()
+      @unknown default:
+        break
+      }
+    }
+  }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    if let shortcutItem = options.shortcutItem {
+      shortcutItemToProcess = shortcutItem
+    }
+    
+    let sceneConfiguration = UISceneConfiguration(name: "Custom Configuration", sessionRole: connectingSceneSession.role)
+    sceneConfiguration.delegateClass = CustomSceneDelegate.self
+    
+    return sceneConfiguration
+  }
+}
+
+class CustomSceneDelegate: UIResponder, UIWindowSceneDelegate {
+  func windowScene(_ windowScene: UIWindowScene, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+    shortcutItemToProcess = shortcutItem
   }
 }
---
+++ ChatGPT/ChatGPT/Application/QuickActionSettings.swift
@@ -0,0 +1,32 @@
+//
+//  Action.swift
+//  ChatGPT
+//
+//  Created by Wenyan Zhao on 2023/8/18.
+//
+
+import UIKit
+
+class QuickActionSettings: ObservableObject {
+  enum QuickAction: Hashable {
+    case newChat
+  }
+  
+  @Published var quickAction: QuickAction? = nil
+  
+  func addQuickActions() {
+    var newChatUserInfo: [String: NSSecureCoding] {
+      return ["name" : "chat" as NSSecureCoding]
+    }
+    
+    UIApplication.shared.shortcutItems = [
+      UIApplicationShortcutItem(
+        type: "New Chat",
+        localizedTitle: "New Chat",
+        localizedSubtitle: "",
+        icon: UIApplicationShortcutIcon(systemImageName: "plus.circle"),
+        userInfo: newChatUserInfo
+      )
+    ]
+  }
+}
